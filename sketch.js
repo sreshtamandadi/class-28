e393329d8418a9867360488de27b7491ea659f6f
sreshtamandadi <Sreshtamandadi@gmail.com>
2020-07-11 11:59:59 +0530

    c28 changes

--- sketch.js
+++ sketch.js
@@ -38,8 +38,17 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+
+    slingShot = new Slingshot(bird.body,{x:200,y:100});
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+    slingShot.fly();
 }
 
 function draw(){
@@ -63,6 +72,5 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    slingShot.display();    
 }
